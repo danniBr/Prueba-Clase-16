431d89049f9174aec47e2f4876a4d9c174a028eb
danniBr <danitza.br@gmail.com>
2018-06-21 20:31:34 -0400

    prueba terminada

--- prueba.rb
+++ prueba.rb
@@ -1,35 +1,72 @@
-opcion = 0
-# 1
-def bla()
+lineas = []
+File.open('notas.csv', 'r') { |file| lineas = file.readlines.each.map(&:chomp) }
 
+# variables
+option = 0
+notaminima = 5.0
+
+# 1
+def crea_promedio(lineas)
+  file = File.new('promedio.txt', 'w')
+  lineas.each do |item|
+    nombre = item.split(', ')[0]
+    notas = item.split(', ')[1..5].each.map(&:to_i)
+    suma = 0
+    notas.each { |nota| suma += nota }
+    promedio = suma.to_f / notas.length
+    file.puts "El promedio de #{nombre} es: #{promedio}"
+  end
+  file.close
+  puts 'Se genero un archivo con los promedios'
 end
 
 # 2
-def bla()
-
+def inasistencia_total(lineas)
+  lineas.each do |item|
+    nombre = item.split(', ')[0]
+    notas = item.split(', ')[1..5].each.map(&:to_i) # 0 = eran "A"
+    total = notas.count(0)
+    puts "#{nombre} tiene #{total} inasistencias" if notas.count(0) >= 1
+  end
+  return nil
 end
 
 # 3
-def bla()
+def alumnos_aprobados(lineas, notaminima) 
+  puts 'Ingrese nota minima de aprobación'
+  notaminima = gets.chomp.to_f
 
+  lineas.each do |item|
+    nombre = item.split(', ')[0]
+    notas = item.split(', ')[1..5].each.map(&:to_i)
+    suma = 0
+    notas.each { |nota| suma += nota }
+    promedio = suma.to_f / notas.length
+      puts "#{nombre} aprobado con nota #{promedio}" if promedio >= notaminima
+  end
+  return nil
 end
 
-while opcion != 4
+# menu
+while option != 4
+  puts '----------------'
   puts 'Elige una opción'
-  puts ''
-  puts 'Ingresa 2 para '
-  puts 'Ingresa 3 para '
-  puts 'Ingresa 4 para'
-  opcion = gets.chomp
+  puts 'Ingresa 1 para generar promedios'
+  puts 'Ingresa 2 para ver inasistencias'
+  puts 'Ingresa 3 para ver almunos aprobados'
+  puts 'Ingresa 4 para salir'
+  option = gets.chomp
 
-  puts case opcion
-  when '1'
-    bla()
-  when '2'
-    bla()
-  when '3'
-    bla()
-  else
-    exit
-  end
+  puts case option
+       when '1'
+         crea_promedio(lineas)
+       when '2'
+         inasistencia_total(lineas)
+       when '3'
+         alumnos_aprobados(lineas, notaminima)
+       when '4'
+         exit
+       else
+         puts 'Ingresa una opción valida'
+       end
 end
